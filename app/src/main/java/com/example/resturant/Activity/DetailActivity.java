ed037ac0006ce51c98b59511592f9054e100de84
leenmajdi003 <2134896@std.hu.edu.jo>
2024-09-28 00:17:43 +0300

    new10

--- app/src/main/java/com/example/resturant/Activity/DetailActivity.java
+++ app/src/main/java/com/example/resturant/Activity/DetailActivity.java
@@ -9,15 +9,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.resturant.R;
+import com.example.resturant.databinding.ActivityDetailBinding;
+import com.example.resturant.databinding.ActivityMainBinding;
 
 public class DetailActivity extends AppCompatActivity {
-
+ActivityDetailBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding= ActivityDetailBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_detail);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
